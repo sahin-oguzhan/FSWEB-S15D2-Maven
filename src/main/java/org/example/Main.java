1cfc395b59db3980414ff0b0838b32dc9dad48b0
sahin-oguzhan <oguzsahiinn@gmail.com>
2025-07-17 00:45:46 +0300

    Add task management entities and update main logic
    Introduced Priority, Status, Task, TaskData, and StringSet classes to support task management features. Updated Main.java to import new entities and removed placeholder code. Adjusted ResultAnalyzer to use a new userId. Minor test improvements in MainTest.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.entity.Priority;
+import org.example.entity.Status;
+import org.example.entity.Task;
+import org.example.entity.TaskData;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
---
+++ src/main/java/org/example/entity/TaskData.java
@@ -0,0 +1,62 @@
+package org.example.entity;
+
+import java.util.*;
+
+public class TaskData {
+    private Set<Task> annsTasks;
+    private Set<Task> bobsTasks;
+    private Set<Task> carolsTasks;
+    private Set<Task> unassignedTasks;
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTasks, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    public Set<Task> getTasks(String name) {
+        switch (name.toLowerCase()) {
+            case "ann":
+                return annsTasks;
+            case "bob":
+                return bobsTasks;
+            case "carol":
+                return carolsTasks;
+            case "all":
+                return getUnion(annsTasks, bobsTasks, carolsTasks);
+            default:
+                return Collections.emptySet();
+        }
+    }
+
+    public Set<Task> getUnion(Set<Task>... sets) {
+        HashSet<Task> allTasks = new LinkedHashSet<>();
+        for (Set<Task> task : sets) {
+            allTasks.addAll(task);
+        }
+        return allTasks;
+    }
+
+    public Set<Task> getIntersection(Set<Task> set1, Set<Task> set2) {
+        Set<Task> intersect = new HashSet<>(set1);
+        intersect.retainAll(set2);
+        return intersect;
+    }
+
+    public Set<Task> getDifferences(Set<Task> set1, Set<Task> set2) {
+        Set<Task> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        return difference;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskData{" +
+                "annsTasks=" + annsTasks +
+                ", bobsTasks=" + bobsTasks +
+                ", carolsTasks=" + carolsTasks +
+                ", unassignedTasks=" + unassignedTasks +
+                '}';
+    }
+}
